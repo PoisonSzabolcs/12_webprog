9959957921671491e42ef6efc3773534cab22208
PoisonSzabolcs <kovacsszabolcs012@gmail.com>
2025-11-17 14:12:45 +0100

    7 topjoy fetch

--- 7_topjoy_fetch/script.js
+++ 7_topjoy_fetch/script.js
@@ -6,8 +6,25 @@ window.addEventListener('DOMContentLoaded', async ()=>{
     try {
         const uzik = await fetch('./uzik.csv');
         console.log(uzik);   
+        const uziktest= (await uzik.text())        
+                        .split('\n')
+                        .map(sor => sor.trim());
+
+        uziktest.forEach(uzi => {
+            messages.unshift(uzi);
+        })
+        console.log(messages)
+
+
+        messageBox.textContent = messages[Math.floor(Math.random() * messages.length)]
     } catch (error) {
+
+
         console.log(error);
         
     }
 })
+
+messageButton.addEventListener('click', ()=>{
+    messageBox.textContent = messages[Math.floor(Math.random() * messages.length)]
+} )
