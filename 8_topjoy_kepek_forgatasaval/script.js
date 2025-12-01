0c6f564b2b241eb05d9302471fbb3a31794760e4
PoisonSzabolcs <kovacsszabolcs012@gmail.com>
2025-12-01 14:29:28 +0100

    8 topjoy done

--- 8_topjoy_kepek_forgatasaval/script.js
+++ 8_topjoy_kepek_forgatasaval/script.js
@@ -1,54 +1,57 @@
-const szoveg = document.querySelector('#szoveg');
-const tarolo = document.querySelector('.tarolo')
-const kupak = document.querySelector('#kupak');
+const szoveg = document.querySelector("#szoveg");
+const tarolo = document.querySelector(".tarolo");
+const kupak = document.querySelector("#kupak");
 const messages = [];
 
-let nyitva = false;
+let zarvaVan = true;
 
-window.addEventListener('DOMContentLoaded', async ()=>{
-    
+window.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const uzik = await fetch("./uzik.csv");
+    console.log(uzik);
+    const uziktest = (await uzik.text()).split("\n").map((sor) => sor.trim());
 
-    try {
-        const uzik = await fetch('./uzik.csv');
-        console.log(uzik);   
-        const uziktest= (await uzik.text())        
-                        .split('\n')
-                        .map(sor => sor.trim());
+    uziktest.forEach((uzi) => {
+      messages.unshift(uzi);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-        uziktest.forEach(uzi => {
-            messages.unshift(uzi);
-        })
-       
+const animateCap = async (start, end) => {
+  const step = start < end ? 1 : -1;
 
-
-       
-    } catch (error) {
-
-
-        console.log(error);
-        
-    }
-})
-
-kupak.addEventListener('click', async () =>{
-
-    let uzi =  messages[Math.floor(Math.random() * messages.length)];
-
-    if(!nyitva){
-       
-    for (let index = 0; index < 11; ) {
-        setTimeout(() => {
-            console.log(kupak.src)
-            index++
-        }, 5000)
-        
+  for (let index = start; index !== end + step; index += step) {
+    await new Promise((resolve) => setTimeout(resolve, 13));
+    if (step === -1) {
+      szoveg.textContent = " ";
     }
 
-      
-    nyitva = true;
+    kupak.src = `./images/bottlecap_${index}.png`;
+  }
 
-    console.log(nyitva)
+  //console.log(step);
 
+  if (step === 1) {
+    await TextShow();
+  }
+};
 
-    }
-})
+const TextShow = () => {
+  const random = Math.floor(Math.random() * messages.length);
+  //console.log("megy");
+  szoveg.textContent = messages[random];
+
+  //console.log();
+};
+
+tarolo.addEventListener("click", async () => {
+  if (zarvaVan) {
+    await animateCap(0, 10);
+    zarvaVan = false;
+  } else {
+    await animateCap(10, 0);
+    zarvaVan = true;
+  }
+});
